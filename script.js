d1dee20f703ec8fb7c5ebdf284b9e728bafb7d73
melpelham <melissajpelham@gmail.com>
2026-09-01 09:26:29 +0930

    added animation

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+const envelope = document.getElementById("envelope");
 const card = document.getElementById("card");
+const cardWrapper = document.getElementById("cardWrapper");
+const instructions = document.getElementById("instructions");
 
-card.addEventListener("click", function () {
+let envelopeOpened = false;
+
+
+/* =========================
+    OPEN ENVELOPE
+========================= */
+
+envelope.addEventListener("click", function () {
+
+    if (envelopeOpened) {
+        return;
+    }
+    envelopeOpened = true;
+    envelope.classList.add("open");
+    instructions.textContent =
+        "Watch the card come out 💌";
+    /*
+        The card starts BEHIND
+        the front of the envelope.
+        After it has travelled upward,
+        bring it to the front before
+        it comes back down.
+    */
+    setTimeout(function () {
+        cardWrapper.style.zIndex = "6";
+        instructions.textContent =
+            "Tap the card to open 💌";
+    }, 1750);
+});
+
+
+/* =========================
+    OPEN CARD
+========================= */
+
+card.addEventListener("click", function (event) {
+    event.stopPropagation();
+    if (!envelopeOpened) {
+        return;
+    }
     card.classList.toggle("open");
+    if (card.classList.contains("open")) {
+        instructions.textContent =
+            "Tap the card to close 💙";
+    } else {
+        instructions.textContent =
+            "Tap the card to open 💌";
+    }
 });
